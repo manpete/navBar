652f1710b887c07bdade1873bf181cf36711e81f
Manuel Peters <manuelpeters.cl@gmail.com>
2021-09-06 19:40:18 -0300

    navBar

---
+++ navbar/src/App.js
@@ -0,0 +1,19 @@
+
+import NavBar from './components/NavBar/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+export default function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <h1>hola soy app</h1>
+    </>
+
+  );
+
+
+}
+
+
